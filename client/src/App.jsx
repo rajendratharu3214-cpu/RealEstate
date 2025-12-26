29a95fb020a5de3c00ee8b05b31bad5f4e98d630
Rajendra Tharu <rajendratharu3214@gmail.com>
2025-12-26 23:59:12 +0545

    4. Created and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import About from './pages/About.jsx';
 import Profile from './pages/Profile.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
-
-
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1 className='flex '>
+            <span className='text-slate-500'>Rajendra</span>
+            <div className="text-slate-700">Estate</div>
+        </h1>
+    </div>
+  )
+}
+
+export default Header
